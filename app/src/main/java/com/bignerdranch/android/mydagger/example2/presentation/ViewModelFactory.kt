cef614c9f63ae5fbb6a96d10cf4ab5407d2c3b4c
aleksvvv <adm-shop-001@yandex.ru>
2023-04-02 22:19:49 +0300

    Provider

--- app/src/main/java/com/bignerdranch/android/mydagger/example2/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/bignerdranch/android/mydagger/example2/presentation/ViewModelFactory.kt
@@ -2,13 +2,16 @@ package com.bignerdranch.android.mydagger.example2.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.bignerdranch.android.mydagger.example2.data.di.ApplicationScope
 import com.bignerdranch.android.mydagger.example2.domain.ExampleUseCase
 import javax.inject.Inject
+import javax.inject.Provider
 
+@ApplicationScope
 class ViewModelFactory @Inject constructor(
-    private val viewModels: @JvmSuppressWildcards Map<String,ViewModel>
+    private val viewModelProvider: @JvmSuppressWildcards Map<String,Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return viewModels[modelClass.simpleName] as T
+        return viewModelProvider[modelClass.simpleName]?.get() as T
     }
 }
